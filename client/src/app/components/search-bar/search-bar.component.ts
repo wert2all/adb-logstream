7e15d6b757e79b0a8b4fdc05fb4eddaa1581b90f
Serhii Shvets <wert2all@gmail.com>
2026-06-13 11:31:06 +0300

    fix: update seaach filter to store

--- client/src/app/components/search-bar/search-bar.component.ts
+++ client/src/app/components/search-bar/search-bar.component.ts
@@ -1,5 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { LogStateService } from '../../services/log-state.service';
+import { Store } from '@ngrx/store';
+import { appActions } from '../../store/app.actions';
+import { appFeature } from '../../store/app.redusers';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,15 +10,22 @@ import { LogStateService } from '../../services/log-state.service';
   templateUrl: './search-bar.component.html',
 })
 export class SearchBarComponent {
+  private store = inject(Store);
+  protected query = this.store.selectSignal(appFeature.selectQueryString);
+
   logState = inject(LogStateService);
 
   onInput(event: Event): void {
-    const value = (event.target as HTMLInputElement).value;
-    this.logState.setSearchQuery(value);
+    const query = (event.target as HTMLInputElement).value;
+    this.logState.setSearchQuery(query);
+
+    this.store.dispatch(appActions.setQuery({ query }));
   }
 
   clearSearch(): void {
     this.logState.setSearchQuery('');
     document.getElementById('search-input')?.blur();
+
+    this.store.dispatch(appActions.cleanQuery());
   }
 }
