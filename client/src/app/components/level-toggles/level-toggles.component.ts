31eea474112f8f5602a74d22a8bc501943025a94
Serhii Shvets <wert2all@gmail.com>
2026-06-17 14:15:33 +0300

    fix: remove useless log-state service

--- client/src/app/components/level-toggles/level-toggles.component.ts
+++ client/src/app/components/level-toggles/level-toggles.component.ts
@@ -1,6 +1,4 @@
 import { Component, computed, inject } from '@angular/core';
-import { LogStateService } from '../../services/log-state.service';
-import { LocalStorageService } from '../../services/local-storage.service';
 import { Level, LevelNames } from '../../app.types';
 import { Store } from '@ngrx/store';
 import { streamFeature } from '../../store/stream/stream.redusers';
@@ -16,9 +14,6 @@ export class LevelTogglesComponent {
   private store = inject(Store);
   private levelFilters = this.store.selectSignal(streamFeature.selectLevelFilters);
 
-  protected logState = inject(LogStateService);
-  protected localStorage = inject(LocalStorageService);
-
   protected levels = computed(() => {
     const filters = this.levelFilters();
     return Object.keys(LevelNames).map((key) => ({
@@ -28,8 +23,6 @@ export class LevelTogglesComponent {
   });
 
   toggleLevel(level: string): void {
-    this.logState.toggleLevel(level);
-
     this.store.dispatch(streamActions.toggleLevel({ level: level as Level }));
   }
 }
--- client/src/app/components/search-bar/search-bar.component.ts
+++ client/src/app/components/search-bar/search-bar.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { LogStateService } from '../../services/log-state.service';
 import { Store } from '@ngrx/store';
 import { streamActions } from '../../store/stream/stream.actions';
 import { streamFeature } from '../../store/stream/stream.redusers';
@@ -12,20 +11,15 @@ import { streamFeature } from '../../store/stream/stream.redusers';
 export class SearchBarComponent {
   private store = inject(Store);
   protected query = this.store.selectSignal(streamFeature.selectQueryString);
-
-  logState = inject(LogStateService);
+  protected searchQuery = this.store.selectSignal(streamFeature.selectQueryString);
 
   onInput(event: Event): void {
     const query = (event.target as HTMLInputElement).value;
-    this.logState.setSearchQuery(query);
-
     this.store.dispatch(streamActions.setQuery({ query }));
   }
 
   clearSearch(): void {
-    this.logState.setSearchQuery('');
     document.getElementById('search-input')?.blur();
-
     this.store.dispatch(streamActions.cleanQuery());
   }
 }
